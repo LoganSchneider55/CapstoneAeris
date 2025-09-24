ab7bc6b7983f71b30f22326f1f2fa68536e6a2af
Logan Schneider <126029606+LoganSchneider55@users.noreply.github.com>
2025-09-23 23:48:40 -0400

    Add CORS main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
 from .routers import readings, devices
 from .database import check_db
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Aeris API", version="0.2.0")
+# Cors
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://localhost:3001",
+        # add your deployed dashboard origin here later if needed
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],   # allows GET, POST, PATCH, OPTIONS, etc.
+    allow_headers=["*"],   # allows Authorization, Content-Type, etc.
+)
 
 @app.get("/healthz")
 def healthz():
